2fedd1f9a48d0848a22431d64c37e5ec2ca5e76a
subru-37 <easwaransubramaniOO7@gmail.com>
2022-07-12 15:02:36 +0530

    picture compatobilit editing part1

--- src/Components/Component5.js
+++ src/Components/Component5.js
@@ -3,16 +3,19 @@ import React from 'react';
 export default function Component3() {
   return (
     <div className="content1" id="about">
-        <div className="about-us">
-            <h1>About HER-O</h1>
-            <p>Her-o comes from the word hero but its her-o representing the majority of sexually abused victims. 
-              We were deeply disturbed at the frequency and intensity of sexual exploitation taking place within the limited space of Pattambi . 
-              That when J can into the equation . J is the unfortunate victim of one of the most gruesome rape case in India . 
-              Her restless soul has decided to take an action on what has been neglected for so long . 
-              <br/>
-              Her-o is a website that acts as a medium to connect people who are dealing with sexual exploitation. Pattambi has reached its zenith with this cause apparently people can’t control their thoughts. J is advocating for rights for people who are victims of this inhumane act. 
-              </p>
-        </div>
+    <div className="bgm">
+    <div className="about-us">
+              <h1>About HER-O</h1>
+              <p>Her-o comes from the word hero but its her-o representing the majority of sexually abused victims. 
+                We were deeply disturbed at the frequency and intensity of sexual exploitation taking place within the limited space of Pattambi . 
+                That when J can into the equation . J is the unfortunate victim of one of the most gruesome rape case in India . 
+                Her restless soul has decided to take an action on what has been neglected for so long . 
+                <br/>
+                Her-o is a website that acts as a medium to connect people who are dealing with sexual exploitation. Pattambi has reached its zenith with this cause apparently people can’t control their thoughts. J is advocating for rights for people who are victims of this inhumane act. 
+                </p>
+          </div>
+    </div>
+          
       </div>
   );
 }
--- src/Components/Component8.js
+++ src/Components/Component8.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
 export default function Component8() {
   return (
     <div className="sample">
         <div className="s1">
+          <img src='https://i.imgur.com/CMRAtRs.png' className='HERO'></img>
         </div>
         <div className="s2">
+          <img src='https://i.imgur.com/CMRAtRs.png' className='HERO'></img>
         </div>
       </div>
   );
