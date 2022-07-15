4a94efa5b6e8443d36cc4202776bd1e5bd933a56
subru-37 <easwaransubramaniOO7@gmail.com>
2022-07-15 22:33:08 +0530

    modifying content

--- src/Components/Component9.js
+++ src/Components/Component9.js
@@ -10,11 +10,11 @@ export default function Component9() {
                     Meet  Pattambi's First Female Superhero Who Fights Sexual Violence 
             </h1>
             <p>
-                Pattambi's first female comic book superhero, Her-o is a rape survivor. 
-                She was raped at a young age and consequently shamed by her family and community and had been tortured physically, and mentally over and over again by her abuser. She wishes that no one goes through the horror she went through. 
-                An Indian sporting superhero is here to save the day of Pattambi and spread awareness about sexual abuse-related issues.
+                Pattambi's first female comic book superhero, Her-o is an advocate for people who had been victims of sexual abuse.  
+                She has seen people be tortured physically, and mentally over and over again whether its in the news or its someone she knows and she wishes that no one goes through the horror she witnessed.
+                HER-O is here to save the day of Pattambi and spread awareness about sexual abuse-related issues.
                 <br/>
-                No good girl walks alone at night that what they say. That's where the idea began. She realised that rape and sexual violence in Pattambi was a cultural issue, and that it was backed by patriarchy, misogyny and people's perceptions. 
+                No good girl walks alone at night that what they say. She realised that rape and sexual violence in Pattambi was a cultural issue, and that it was backed by patriarchy, misogyny and people's perceptions. 
             </p>
         </div>
     </div>
